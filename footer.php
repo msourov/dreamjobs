7b2a1b1322f052198e8a16c4b0dec4b07174e76b
msourov <37919132+msourov@users.noreply.github.com>
2022-12-11 14:58:42 +0600

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,93 @@
+<footer class="footer-wrapper">
+
+    <div class="main-footer">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-sm-12 col-md-9">
+
+                    <div class="row">
+
+                        <div class="col-sm-6 col-md-4">
+
+                            <div class="footer-about-us">
+                                <h5 class="footer-title">About Dream Jobs</h5>
+                                <p>Dream Jobs is a job portal, online job management system developed by XYZ.</p>
+
+                            </div>
+
+                        </div>
+
+                        <div class="col-sm-6 col-md-5 mt-30-xs">
+                            <h5 class="footer-title">Quick Links</h5>
+                            <ul class="footer-menu clearfix">
+                                <li><a href="./">Home</a></li>
+                                <li><a href="job-list.php">Job List</a></li>
+                                <li><a href="employers.php">Employers</a></li>
+                                <li><a href="employees.php">Employees</a></li>
+                                <li><a href="contact.php">Contact Us</a></li>
+                                <li><a href="#">Go to top</a></li>
+
+                            </ul>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col-sm-12 col-md-3 mt-30-sm">
+
+                    <h5 class="footer-title">Dream Jobs Contact</h5>
+
+                    <p>Address : Dhaka, Bangladesh</p>
+                    <p>Email : <a href="mailto:dreamjobs@gmail.com">dreamjobs@gmail.com</a></p>
+                    <p>Phone : <a href="tel:+1234567890">+1234567890</a></p>
+
+
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="bottom-footer">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-sm-4 col-md-4">
+
+                    <p class="copy-right">&#169; Copyright <?php echo date('Y'); ?> XYZ</p>
+
+                </div>
+
+                <div class="col-sm-4 col-md-4">
+
+                    <ul class="bottom-footer-menu">
+                        <li><a>Developed by XYZ</a></li>
+                    </ul>
+
+                </div>
+
+                <div class="col-sm-4 col-md-4">
+                    <ul class="bottom-footer-menu for-social">
+                        <li><a href="<?php echo "$tw"; ?>"><i class="ri ri-twitter" data-toggle="tooltip" data-placement="top" title="twitter"></i></a></li>
+                        <li><a href="<?php echo "$fb"; ?>"><i class="ri ri-facebook" data-toggle="tooltip" data-placement="top" title="facebook"></i></a></li>
+                        <li><a href="<?php echo "$ig"; ?>"><i class="ri ri-instagram" data-toggle="tooltip" data-placement="top" title="instagram"></i></a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</footer>
